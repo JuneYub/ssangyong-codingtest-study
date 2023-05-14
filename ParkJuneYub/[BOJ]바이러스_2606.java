d73404483a3da6266ce153993b40245f1d96702b
JuneYub <ajsjdlwj0123@gmail.com>
2023-05-15 00:38:26 +0900

    [BOJ] 바이러스 2606

--- ParkJuneYub/[BOJ]바이러스_2606.java
+++ ParkJuneYub/[BOJ]바이러스_2606.java
@@ -5,7 +5,38 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 import java.util.StringTokenizer;
-
+/*
+ 필요변수
+ 노드 개수
+ 간선 개수
+ ArrayList 의 그래프
+ 불린 배열 비지티드
+ 큐<인티저> 뉴 링크드리스트
+ 
+ 필묘 메소드
+ BFS()
+ 큐가 비어있지 않다면
+ 현재 노드는 q에서 가져오고 (poll)
+ 
+ 그래프에서 현재 노드에 해당하는 리스트의 크기 만큼 반복한다.
+ 다음 노드는 현재 노드의 리스트에서 순차적으로 가져오고
+ 만약 방문하지 않았떤 거라면 방문하고 큐에 추가한다.
+ 
+ 메인 함수
+ 간선과 노드의 개수 설정해주고
+ 그래프는 간선개수+1 만큼 어레이 리스트 배열로 구성한다.
+ 비지티드도 간선개수+1 만큼 배열로 만든다.
+ 
+ 1부터 간선 개수만큼 그래프[i]에게 새로운 arrayList를 주고
+ 
+ 스타트Points[] endPoints[] 간선 개수만큼 배열 설정하고
+ 0부터 간선 개수만큼 스타트Points[] endPoints[] 에 간선 연결해준다.
+ 
+ 이를 이용해 그래프.add 를 활용해 그래프의 노드를 이어준다. <= 링크드 리스트에 추가하는 거임
+ 
+ rootVertex = 1 큐에 추가하고 비지티드 하고 
+ BFS 실행
+*/
 
 
 public class Main {
